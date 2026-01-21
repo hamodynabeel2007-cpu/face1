37447233e7d882584e620e631e1224a0371c5d11
hamodynabeel2007-cpu <hamodynabeel2007@gmail.com>
2026-01-21 19:14:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
 
-image = Image.open(uploaded_file)
-st.image(image, caption='Uploaded Image', use_column_width=True)
+ image = Image.open(uploaded_file)
+ st.image(image, caption='Uploaded Image', use_column_width=True)
 
 
 img_array = np.array(image.convert('RGB'))
